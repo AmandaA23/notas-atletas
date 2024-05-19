9a78f9668630511c0934ae899b620565e5f3e33c
AmandaA23 <112908459+AmandaA23@users.noreply.github.com>
2024-05-19 10:26:11 -0300

    Update and rename index.html to notas-atletas.js

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>
         
 function calcularMedia(atletas) {
   atletas.forEach(atleta => {
@@ -24,16 +15,29 @@ function calcularMedia(atletas) {
     // Calcular a média
     let media = soma / notasEliminadas.length;
 
-    // Exibir os resultados
+    let atletas = [
+ {
+   nome: "Cesar Abascal",
+   notas: [10, 9.34, 8.42, 10, 7.88]
+ },
+ {
+   nome: "Fernando Puntel",
+   notas:  [8, 10, 10, 7, 9.33]
+ },
+ {
+   nome: "Daiane Jelinsky",
+   notas: [7, 10, 9.5, 9.5, 8]
+ },
+ {
+   nome: "Bruno Castro",
+   notas: [10, 10, 10, 9, 9.5]
+ }
+];
+
+ // resultados no console
 
     console.log(`Atleta: ${atleta.nome}`);
     console.log(`Notas Obtidas: ${atleta.notas.join(",")}`);
     console.log(`Média Válida: ${media}`);
-    console.log('');
   });
 }
-
-    </script>
-    
-</body>
-</html>
